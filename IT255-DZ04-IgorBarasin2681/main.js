b29457912a1a34a96ca4cb5917a6b832d5b4329e
igorbarasin2681 <barasin.igor@gmail.com>
2017-10-18 08:59:27 +0200

    IT255-DZ04-IgorBarasin2681

---
+++ IT255-DZ04-IgorBarasin2681/main.js
@@ -0,0 +1,23 @@
+var body = document.getElementsByTagName("BODY")[0];
+
+function original() {
+	body.style.backgroundColor = "BurlyWood";
+	body.style.fontFamily = "Arial";
+	body.style.fontWeight = "100";
+	body.style.fontStyle = "normal";
+};
+
+function vintage() {
+	body.style.backgroundColor = "GoldenRod";
+	body.style.fontFamily = "Goudy Old Style";
+	body.style.fontWeight = "bold";
+	body.style.fontStyle = "normal";
+};
+
+function cool() {
+	body.style.backgroundColor = "Navy";
+	body.style.fontFamily = "Georgia";
+	body.style.fontWeight = "100";
+	body.style.fontStyle = "italic";
+};
+
